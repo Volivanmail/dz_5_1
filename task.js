13acba3a704f4b1c83e8803eceab84d66303f54c
Volivanmail <84474430+Volivanmail@users.noreply.github.com>
2022-08-03 03:58:09 +0300

    commit 03.08

--- task.js
+++ task.js
@@ -1,23 +1,11 @@
-const p = document.getElementById('timer')
-let num = Number(p.textContent)
-
-function winner() {
-    setInterval(() => {p.textContent = Number(p.textContent) - 1}, 1000)
-    if (timerId == 0) {
-        clearInterval();
-        alert('Вы победили в конкурсе');
-    }
-}
-
-winner();
+const timer = document.getElementById('timer');
 
 
-// let i = 0;
+let timerId = setInterval(() => {
+   timer.textContent = Number(timer.textContent) - 1;
+   if (Number(timer.textContent) < 0) {
+      clearInterval(timerId);
+      alert('Вы победили в конкурсе');
+   }; 
+}, 1000);
 
-// let timerId = setInterval(function() {
-// 	console.log(++i);
-	
-// 	if (i >= 10) {
-// 		clearInterval(timerId);
-// 	}
-// }, 1000);
